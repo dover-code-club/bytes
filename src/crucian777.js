8f93e19add2980bfdb63bb530e98e52e2790b7f8
Abhay Ramnarayan <ramna50916@Abhays-MacBook-Air.local>
2021-02-23 16:23:07 +0800

    Add author card grrr

---
+++ src/crucian777.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Box } from "@chakra-ui/react"
+export default class claby2 extends React.Component {
+    render() {
+        return <Box bg="tomato" w="100%" p={4} color="blue">
+        crucian777
+      </Box>
+  }
+}
+
