e32efa84cc9585cbb18a5c3bf259da7701f752b7
Basant9938 <bruce9938@gapps.uwcsea.edu.sg>
2021-02-23 16:24:26 +0800

    Adding Basant's author card

---
+++ src/Basant9938.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Box} from "@chakra-ui/react"
+
+export default class Basant9938 extends React.Component{
+    render(){
+        return <Box color="blue">
+            Tnasab Eol
+            Baby shoes
+            Never worn
+        </Box>
+    }
+}  
