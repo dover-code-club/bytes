34646ee4a1c646c19c6fb10bbb8f654373229793
justtryingthingsout <senshinkawaguchi123@gmail.com>
2021-02-23 16:23:11 +0800

    Added author box

---
+++ src/justtryingthingsout.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Box } from "@chakra-ui/react"
+import { Heading } from "@chakra-ui/react"
+export default class justtryingthingsout extends React.Component {
+    render() {
+        return <Box bg="pink" w="100%" p={4} color="black"><Heading>@justtryingthingsout</Heading></Box>
+    }
+}
