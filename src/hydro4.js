91e86b0b36f0c7589c51e5cf34a21e8c4b91c3da
hydro4 <46918065+Hydro4@users.noreply.github.com>
2021-02-23 16:21:37 +0800

    feat: add author card

---
+++ src/hydro4.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Box } from "@chakra-ui/react"
+
+export default class hydro4 extends React.Component {
+    render() {
+        return <Box bg="tomato" w="100%" p={4} color="white">
+        Author: Hydro4
+        </Box>
+    }
+}
