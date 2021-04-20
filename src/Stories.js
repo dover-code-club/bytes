c5cd4cfe0032a96e962abfa4b4d178e317289d62
Abhay Ramnarayan <ramna50916@Abhays-MacBook-Air.local>
2021-04-20 16:13:27 +0800

    unfortunately i got told by hr im not allowed to use storieses as a plural of stories because apparently its already a plural

--- src/Stories.js
+++ src/Stories.js
@@ -1,16 +1,16 @@
 import React from "react";
-import { storageKey, Box, SimpleGrid, Grid} from "@chakra-ui/react"
+import { Box, SimpleGrid, Grid} from "@chakra-ui/react"
 import { Image, Heading } from "@chakra-ui/react"
 import { Button, ButtonGroup, Stack } from "@chakra-ui/react"
 import UserStory from "./UserStory";
 export default class Stories extends React.Component {
     getStories(){
-        let Storieses = [];
+        let Stories = [];
         for (let i = 0; i < 10; ++i) {
-            Storieses.push(<UserStory />);
+            Stories.push(<UserStory />);
         }
-        console.log(Storieses);
-        return Storieses;
+        console.log(Stories);
+        return Stories;
     }
     render(){
         return (
--- src/UserStory.js
+++ src/UserStory.js
@@ -5,7 +5,7 @@ export default class UserStory extends React.Component {
     render() {
         return(
             <Button colorScheme="teal" size="md" variant="ghost">
-                <Image src={this.props.src} alt="user69"boxSize="60px" onClick={this.props.onClick}/>
+                <Image src={this.props.src} alt={this.alt.src}boxSize="60px" onClick={this.props.onClick}/>
             </Button>)
     }
 }
